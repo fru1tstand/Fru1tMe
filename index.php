4d53bb96faac7372b83fc7e61b825513caaed9ec
Kodlee Yin <kodlee@kodleeshare.net>
2014-11-01 22:30:29 -0700

    Finished noscript handling page

--- index.php
+++ index.php
@@ -7,15 +7,38 @@
 	</head>
 	
 	<body>
-		This is just the normal text
-		<div id="background">
-			<div id="global-console" class="console">
-				<div class="red">This is some a warning text</div>
-				<div class="blue">This is some info text</div>
-				<div class="green">This text is successful in life</div>
-				<div>This is just regular entry text</div>
-
+		<div id="nojs">
+			<noscript>
+			<div id="nojs_graphic">
+				<div>
+					<div id="nojs_plug_cord"></div>
+					<div class="clearfix"></div>
+					<div id="nojs_plug_head"></div>
+					<div class="nojs_socket_prong"></div>
+					<div class="nojs_socket_pin_spacer"></div>
+					<div class="nojs_socket_prong"></div>
+					<div id="nojs_socket">
+						<div id="nojs_socket_pins">
+							<div class="nojs_socket_pin"></div>
+							<div class="nojs_socket_pin_spacer"></div>
+							<div class="nojs_socket_pin"></div>
+						</div>
+					</div>
+				</div>
 			</div>
+			<div id="nojs_text_push"></div>
+			<div id="nojs_text">
+				<div>
+					<h1>See this?</h1>
+					<h3>This is what you reduce my website to when you disable Javascript...</h3>
+					<h3>Enable it to see why!</h3>
+				</div>
+			</div>
+			</noscript>
+		</div>
+		
+		<div id="background">
+			<div id="global-console" class="console"></div>
 		</div>
 		
 		<script src="/.internal/js/global.js"></script>
