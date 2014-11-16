f62f5f99b8998056c6fede80751dff0a15e1269a
Kodlee Yin <kodlee@kodleeshare.net>
2014-11-15 19:56:56 -0800

    Cleaned up file structure

--- .internal/php/SQL.php
+++ .internal/php/SQL.php
@@ -1,4 +1,20 @@
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/.internal/php/import.php';
+
+/**
+CREATE TABLE `remote_api_calls` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `name` char(64) NOT NULL,
+   `fetch_date` int(11) NOT NULL,
+   `value` text,
+   PRIMARY KEY (`id`,`name`),
+   UNIQUE KEY `id_UNIQUE` (`id`),
+   UNIQUE KEY `name_UNIQUE` (`name`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+ */
+
+/**
+ * Provides a simple singleton connection to the database.
+ */
 class SQL {
 	const HOST = "localhost";
 	const DATABASE = "Fru1tMe";
@@ -13,8 +29,9 @@ class SQL {
 	private static $connection = null;
 	public static function getConnection() {
 		if (is_null(SQL::$connection)) {
-			SQL::$connection = new mysqli(SQL::HOST, SQL::USERNAME, SQL::$pw, SQL::DATABASE, $port, $socket)
+			SQL::$connection = new mysqli(SQL::HOST, SQL::USERNAME, SQL::$pw, SQL::DATABASE);
 		}
+		return SQL::$connection;
 	}
 }
 ?>
