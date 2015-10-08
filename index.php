ab47096efa606a02567f8d026cbb6cea66c523e9
Kodlee Yin <kodlee@kodleeshare.net>
2015-10-08 10:12:53 -0700

    Initial commit

--- index.php
+++ index.php
@@ -13,65 +13,30 @@ if (Page::getBodyOnlyRequest()) {
 
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Fru1tMe</title>
-		<meta charset="UTF-8" />
-		<link href='https://fonts.googleapis.com/css?family=Arimo' rel='stylesheet' type='text/css'>
-		<link href="/.site/css/global.css" rel="stylesheet" type="text/css" />
-		<script src="/.site/js/preload.js"></script>
-	</head>
+<head>
+	<title>Fru1tMe</title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href='https://fonts.googleapis.com/css?family=Raleway'
+		  rel='stylesheet'
+		  type='text/css'>
+	<link href="/.site/styles/compiled/global.css" rel="stylesheet" type="text/css" />
+	<script src="/.site/js/preload.js"></script>
+</head>
 
-	<body>
-		<noscript>
-		<div id="nojs">
-			<div id="nojs_graphic">
-				<div>
-					<div id="nojs_plug_cord"></div>
-					<div class="clearfix"></div>
-					<div id="nojs_plug_head"></div>
-					<div class="nojs_socket_prong"></div>
-					<div class="nojs_socket_pin_spacer"></div>
-					<div class="nojs_socket_prong"></div>
-					<div id="nojs_socket">
-						<div id="nojs_socket_pins">
-							<div class="nojs_socket_pin"></div>
-							<div class="nojs_socket_pin_spacer"></div>
-							<div class="nojs_socket_pin"></div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div id="nojs_text_push"></div>
-			<div id="nojs_text">
-				<div>
-					<h1>See this?</h1>
-					<h3>This is what you reduce my website to when you disable Javascript...</h3>
-					<h3>Enable it to see why!</h3>
-				</div>
-			</div>
-		</div>
-		</noscript>
+<body>
+	<nav>
+		this is the nav!
+	</nav>
 
-		<div id="global-nav">
-			<div id="global-nav-toggle"></div>
-			<a href="/home" class="global-nav-item">Home</a>
-			<a href="/about" class="global-nav-item">About</a>
-			<a href="/projects" class="global-nav-item">Projects</a>
-			<a href="/code" class="global-nav-item">Code</a>
-			<a href="/tools" class="global-nav-item">Tools</a>
-		</div>
+	<div id="global-content">
+		<?php /** @noinspection PhpIncludeInspection */
+		include(Page::getPageLocation(Page::getPageRequest())); ?>
+	</div>
 
-		<div id="global-content">
-			<?php include(Page::getPageLocation(Page::getPageRequest())); ?>
-		</div>
-
-		<div id="background">
-			<div id="global-console" class="console"></div>
-		</div>
-
-		<script src="/.site/js/global.js"></script>
-		<script src="/.site/js/goog_analytics.js"></script>
-	</body>
+	<script src="/.site/php/Pages/_resource/global_js.php"></script>
+	<script src="/.site/js/goog_analytics.js"></script>
+</body>
 </html>
 
 <?php
