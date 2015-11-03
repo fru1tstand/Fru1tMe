dcc2eac7f6da510ed93d097a39ca9dc4ecef5cd1
Kodlee Yin <kodlee@kodleeshare.net>
2015-11-03 00:21:50 -0800

    Spaced landing page text properly on desktop

--- .site/php/fru1tme/html/content/index.php
+++ .site/php/fru1tme/html/content/index.php
@@ -53,34 +53,39 @@ $body = <<<HTML
 	<div class="overlay">
 		<div>
 			<div class="container">
-				<h3>What is this?</h3>
-				<p>
-					Welcome to Fru1tMe!
-					This small space contains a slew of things I've dumped from my brain onto
-					the internet. Feel free to look around and copy things that look neat or
-					interest you. Take a peek at the
-					<a href="/projects" class="inverse">projects</a> page to see things I've done.
-					Or if you're more of a hands-on type of person, check out the
-					<a href="/code" class="inverse">code</a> section to view quirks and other
-					anomalies that I've encountered.
-				</p>
+				<div class="home-container">
+					<h3>What is this?</h3>
+					<p>
+						Welcome to Fru1tMe!
+						This small space contains a slew of things I've dumped from my brain onto
+						the internet. Feel free to look around and copy things that look neat or
+						interest you. Take a peek at the
+						<a href="/projects" class="inverse">projects</a> page to see things I've done.
+						Or if you're more of a hands-on type of person, check out the
+						<a href="/code" class="inverse">code</a> section to view quirks and other
+						anomalies that I've encountered.
+					</p>
 
-				<h3>Who created it?</h3>
-				<p>
-					My name's Kodlee Yin. I'm a college undergraduate at the
-					University of Washington in Seattle. I like to play with everything
-					technology-wise, and currently have a passion for web development.
-					Check out <a href="/resume" class="inverse">my résumé</a> to learn more about
-					me.
-				</p>
+					<h3>Who created it?</h3>
+					<p>
+						My name's Kodlee Yin. I'm a college undergraduate at the
+						University of Washington in Seattle. I like to play with everything
+						technology-wise, and currently have a passion for web development.
+						Check out <a href="/resume" class="inverse">my résumé</a> to learn more about
+						me.
+					</p>
 
-				<h3>How do I get around?</h3>
-				<p>
-					Click that light-blue thing at the top of the page. Heck, you can even open it
-					by <label for="nav-index" class="inverse">clicking here</label> or
-					<label for="nav-index" class="inverse">here</label>. Or maybe even
-					<label for="nav-index" class="inverse">here</label>. Options are always nice :)
-				</p>
+					<h3>How do I get around?</h3>
+					<p>
+						If you're on mobile, click that light-blue thing at the top of the page.
+						Heck, you can even open it
+						by <label for="nav-index" class="inverse">clicking here</label> or
+						<label for="nav-index" class="inverse">here</label>. Or maybe even
+						<label for="nav-index" class="inverse">here</label>. Options are always
+						nice :) <br />
+						Otherwise, on desktop, just click those links on the left.
+					</p>
+				</div>
 			</div>
 		</div>
 	</div>
